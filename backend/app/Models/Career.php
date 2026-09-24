65ddae3a5b68536f40e912f7f084e3d89fd92fdd
Carlos Diego Mariscal Segovia <cardecomarisegovia@gmail.com>
2026-09-24 15:03:42 -0400

    refactor(academic): quita HasFactory de Career y Period

--- backend/app/Models/Career.php
+++ backend/app/Models/Career.php
@@ -4,12 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Career extends Model
 {
-    use HasFactory; 
-
     protected $table = 'carrera';
 
     protected $primaryKey = 'id_carrera';
--- backend/app/Models/Period.php
+++ backend/app/Models/Period.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Period extends Model
 {
-    use HasFactory;
-
     protected $table = 'periodo';
 
     protected $primaryKey = 'id_periodo';
